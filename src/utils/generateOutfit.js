6c373d9ac727db452f8f7d9d538fee4ff312843e
Cupidx0 <107266946+Cupidx0@users.noreply.github.com>
2025-06-29 11:19:03 +0100

    updated backend2

--- src/utils/generateOutfit.js
+++ src/utils/generateOutfit.js
@@ -1,3 +1,3 @@
-fetch("https://outfit-animator.vercel.app/weather?city=London")
+fetch("https://outfit-animator-aclevjfe2-cupidx0s-projects.vercel.app/weather?city=London")
   .then(res => res.json())
   .then(data => console.log(data));
