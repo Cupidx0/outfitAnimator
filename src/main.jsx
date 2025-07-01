28654fe4805619ffe7a998aba31d6ab0882b56c7
Cupidx0 <107266946+Cupidx0@users.noreply.github.com>
2025-07-01 21:46:43 +0100

    updated frontend1.6

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './pages/AuthContext.jsx'
+//createRoot(document.getElementById('root')).render(
+// <StrictMode>
+//   <AuthProvider>
+//     <App />
+//   </AuthProvider>
+// </StrictMode>,
+//)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //<StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>,
+  //</StrictMode>,
 )
