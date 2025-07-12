a5d217edb0d04fbdc6c9954d61eba6947856c806
Cupidx0 <107266946+Cupidx0@users.noreply.github.com>
2025-07-12 09:17:28 +0100

    updated frontend3.4.7

--- outfit-backend/outfitback.py
+++ outfit-backend/outfitback.py
@@ -305,9 +305,10 @@ def generate_outfit_from_closet():
 
             # 🎨 Generate DALL·E 3 Image
             promptt = (
-                f"Fashion illustration of a complete outfit suitable for {weather_tag} weather, "
-                f"consisting of: {prompt_text}. High detail, realistic lighting, plain background, "
-                "modern fashion, professional photoshoot style."
+                f"Fashion illustration of a complete outfit suitable for {weather_tag} weather.\n"
+                f"it should of the: {prompt_text}. High detail, realistic lighting, plain background, "
+                "modern fashion, professional photoshoot style.\n"
+                "suggest an outfit :"
             )
             try:
                 image_response = openai.Image.create(
